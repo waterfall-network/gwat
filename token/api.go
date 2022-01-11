2bbfb71801dd43865ce30705ce85e08852215963
Roman Mazalov <mazalov@waterfall.foundation>
2022-01-11 16:14:34 +0300

    feat: register token APIs in backend
    * Add GetAPIs function for getting apis of token package
    * Register token apis in eth backend
    * Add return in api stubs

--- token/api.go
+++ token/api.go
@@ -3,8 +3,9 @@ package token
 import (
 	"context"
 
-	"github.com/ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/rpc"
 )
 
 type Backend interface{}
@@ -31,22 +32,41 @@ type TokenArgs struct {
 	BaseURI *hexutil.Bytes `json:"baseURI"`
 }
 
-func (s *PublicTokenAPI) TokenCreate(ctx context.Context, args TokenArgs) (hexutil.Bytes, error) {}
+func (s *PublicTokenAPI) TokenCreate(ctx context.Context, args TokenArgs) (hexutil.Bytes, error) {
+	return nil, nil
+}
 
 func (s *PublicTokenAPI) Wrc20Props(ctx context.Context, tokenAddr common.Address) (*wrc20Properties, error) {
+	return nil, nil
 }
 
 func (s *PublicTokenAPI) Wrc20BalanceOf(ctx context.Context, tokenAddr common.Address, ownerAddr common.Address) (*hexutil.Big, error) {
+	return nil, nil
 }
 
 func (s *PublicTokenAPI) Wrc20Transfer(ctx context.Context, tokenAddr common.Address, to common.Address, value hexutil.Big) (bool, error) {
+	return false, nil
 }
 
 func (s *PublicTokenAPI) Wrc20TransferFrom(ctx context.Context, tokenAddr common.Address, from common.Address, to common.Address, value hexutil.Big) (bool, error) {
+	return false, nil
 }
 
 func (s *PublicTokenAPI) Wrc20Approve(ctx context.Context, tokenAddr common.Address, spenderAddr common.Address, value hexutil.Big) (bool, error) {
+	return false, nil
 }
 
 func (s *PublicTokenAPI) Wrc20Allowance(ctx context.Context, tokenAddr common.Address, ownerAddr common.Address, spenderAddress common.Address) (*hexutil.Big, error) {
+	return nil, nil
+}
+
+func GetAPIs(apiBackend Backend) []rpc.API {
+	return []rpc.API{
+		{
+			Namespace: "wat",
+			Version:   "1.0",
+			Service:   NewPublicTokenAPI(apiBackend),
+			Public:    true,
+		},
+	}
 }
