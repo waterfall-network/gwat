8fb193e85310c38c57c6904408e0fc0ffdcb92c9
Roman Mazalov <mazalov@waterfall.foundation>
2022-01-14 17:56:44 +0300

    feat,refactor: WRC-20 and WRC-721 properties and balance of
    Refactor WRC-20 properties, move them into `wat_tokenProperties`
    call.
    
    Add WRC-721 properties into `wat_tokenProperties` call.
    
    Rename call for balance of tokens.
    
    Implement dumb data return for the token properties stub.

--- token/api.go
+++ token/api.go
@@ -20,11 +20,27 @@ func NewPublicTokenAPI(b Backend) *PublicTokenAPI {
 	return &PublicTokenAPI{b}
 }
 
+type wrc721Properties struct {
+	Name   *hexutil.Bytes `json:"name"`
+	Symbol *hexutil.Bytes `json:"symbol"`
+}
+
 type wrc20Properties struct {
-	Name        *hexutil.Bytes `json:"name"`
-	Symbol      *hexutil.Bytes `json:"symbol"`
-	Decimals    *hexutil.Uint  `json:"decimals"`
-	TotalSupply *hexutil.Big   `json:"totalSupply"`
+	wrc721Properties
+	Decimals    *hexutil.Uint `json:"decimals"`
+	TotalSupply *hexutil.Big  `json:"totalSupply"`
+}
+
+type wrc721ByTokenIdProperties struct {
+	TokenURI    *hexutil.Bytes  `json:"tokenURI"`
+	OwnerOf     *common.Address `json:"ownerOf"`
+	GetApproved *common.Address `json:"getApproved"`
+	Metadata    *hexutil.Bytes  `json:"metadata"`
+}
+
+type wrc721TokenProperties struct {
+	wrc721Properties
+	ByTokenId *wrc721ByTokenIdProperties `json:"byTokenId",omitempty`
 }
 
 type TokenArgs struct {
@@ -56,17 +72,53 @@ func (s *PublicTokenAPI) TokenCreate(ctx context.Context, args TokenArgs) (hexut
 	return nil, nil
 }
 
-func (s *PublicTokenAPI) Wrc20Props(ctx context.Context, tokenAddr common.Address) (*wrc20Properties, error) {
-	log.Info("WRC-20 token properties", "tokenAddr", tokenAddr)
-	name := hexutil.Bytes("Test token")
-	symbol := hexutil.Bytes("TST")
+func (s *PublicTokenAPI) TokenProperties(ctx context.Context, tokenAddr common.Address, tokenId *hexutil.Big) (interface{}, error) {
+	name := "Test token"
+	nameBytes := hexutil.Bytes(name)
+	symbol := "TST"
+	symbolBytes := hexutil.Bytes(symbol)
+
+	if tokenId != nil {
+		tokenURI := "https://waterfall.foundation/testtoken/1.json"
+		tokenURIBytes := hexutil.Bytes(tokenURI)
+		ownerOf := "0x3552fea0d44cb11d56210ca8a8f04a69c67ebf48"
+		ownerOfBytes := common.HexToAddress(ownerOf)
+		getApproved := "0x43b5339ea30687e43c39336d96c5f0db278debf6"
+		getApprovedBytes := common.HexToAddress(getApproved)
+		metadata := "{ name: \"Test token\" }"
+		metadataBytes := hexutil.Bytes(metadata)
+
+		log.Info("Return WRC-721 properties by token id", "name", name, "symbol", symbol, "tokenURI", tokenURI, "ownerOf", ownerOf, "getApproved", getApproved, "metadata", metadata)
+		return &wrc721TokenProperties{
+			wrc721Properties{
+				Name:   &nameBytes,
+				Symbol: &symbolBytes,
+			},
+			&wrc721ByTokenIdProperties{
+				TokenURI:    &tokenURIBytes,
+				OwnerOf:     &ownerOfBytes,
+				GetApproved: &getApprovedBytes,
+				Metadata:    &metadataBytes,
+			},
+		}, nil
+	}
+
 	decimals := hexutil.Uint(3)
 	totalSupply := (*hexutil.Big)(big.NewInt(1000))
-	return &wrc20Properties{&name, &symbol, &decimals, totalSupply}, nil
+
+	log.Info("Return WRC-20 properties", "name", name, "symbol", symbol, "decimals", decimals, "totalSupply", totalSupply)
+	return &wrc20Properties{
+		wrc721Properties{
+			Name:   &nameBytes,
+			Symbol: &symbolBytes,
+		},
+		&decimals,
+		totalSupply,
+	}, nil
 }
 
-func (s *PublicTokenAPI) Wrc20BalanceOf(ctx context.Context, tokenAddr common.Address, ownerAddr common.Address) (*hexutil.Big, error) {
-	log.Info("WRC-20 balance of", "tokenAddr", tokenAddr, "ownerAddr", ownerAddr)
+func (s *PublicTokenAPI) TokenBalanceOf(ctx context.Context, tokenAddr common.Address, ownerAddr common.Address) (*hexutil.Big, error) {
+	log.Info("Token balance of", "tokenAddr", tokenAddr, "ownerAddr", ownerAddr)
 	return nil, nil
 }
 
