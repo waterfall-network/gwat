58a9e22b3e216793f9455dfc33c775cfd23b353b
Roman Mazalov <mazalov@waterfall.foundation>
2022-01-26 18:36:28 +0300

    feat: add token standard and operation type
    Add type for storing token operation and standard in `token`
    package.
    
    Define constants with token operations and standards.
    
    Define interface for token operation.

---
+++ token/token.go
@@ -0,0 +1,28 @@
+package token
+
+import "github.com/ethereum/go-ethereum/common"
+
+// Token standard
+type Std uint
+
+const (
+	StdWRC20  = 20
+	StdWRC721 = 721
+)
+
+// Token operation code
+type OpCode uint
+
+// Token operation codes use invalid op codes of EVM instructions to prevent clashes.
+const (
+	OpCreate       = 0x0C
+	OpApprove      = 0x0D
+	OpTransfer     = 0x1E
+	OpTransferFrom = 0x1F
+)
+
+type Operation interface {
+	OpCode() OpCode
+	Standard() Std
+	Address() common.Address // Token address
+}
