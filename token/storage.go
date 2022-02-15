997d5832c6d50f84edde7f67278a22568f3f0e16
Roman Mazalov <mazalov@waterfall.foundation>
2022-02-15 14:54:15 +0300

    feat,refactor: implement reading for token storage
    Implement methods for reading from token storage.
    
    Refactor write method. Move the method logic into a separate
    function to reuse in read method.

--- token/storage.go
+++ token/storage.go
@@ -12,11 +12,14 @@ const SlotSize = 32
 type Storage struct {
 	pos   int
 	slots []common.Hash
+
+	readSlot func(slot int) common.Hash
 }
 
-func NewStorage() Storage {
+func NewStorage(readSlot func(int) common.Hash) Storage {
 	return Storage{
-		slots: make([]common.Hash, 1),
+		slots:    make([]common.Hash, 1),
+		readSlot: readSlot,
 	}
 }
 
@@ -49,19 +52,68 @@ func (s *Storage) Write(b []byte) (int, error) {
 }
 
 func (s *Storage) write(b []byte) {
+	s.do(b, func(slotSlice, bSlice []byte) {
+		copy(bSlice, slotSlice)
+	}, func(slot int) common.Hash {
+		return common.Hash{}
+	})
+}
+
+func (s *Storage) ReadUint8() uint8 {
+	buf := make([]byte, 1)
+	s.read(buf)
+	return buf[0]
+}
+
+func (s *Storage) ReadUint16() uint16 {
+	buf := make([]byte, 2)
+	s.read(buf)
+	return binary.BigEndian.Uint16(buf)
+}
+
+func (s *Storage) ReadUint64() uint64 {
+	buf := make([]byte, 8)
+	s.read(buf)
+	return binary.BigEndian.Uint64(buf)
+}
+
+func (s *Storage) ReadUint256() *big.Int {
+	buf := make([]byte, 32)
+	s.read(buf)
+	v := new(big.Int)
+	return v.SetBytes(buf)
+}
+
+func (s *Storage) ReadBytes() []byte {
+	l := s.ReadUint64()
+	b := make([]byte, l)
+	s.read(b)
+	return b
+}
+
+func (s *Storage) read(b []byte) {
+	s.do(b, func(slotSlice, bSlice []byte) {
+		copy(slotSlice, bSlice)
+	}, s.readSlot)
+}
+
+func (s *Storage) do(b []byte, action func(slotSlice, bSlice []byte), getSlot func(int) common.Hash) {
 	if s.pos >= SlotSize {
-		s.slots = append(s.slots, common.Hash{})
+		s.slots = append(s.slots, getSlot(len(s.slots)))
 		s.pos = 0
 	}
 
 	if s.pos+len(b) > SlotSize {
 		size := SlotSize - s.pos
-		defer s.write(b[size:])
-		copy(s.slots[len(s.slots)-1][s.pos:], b[:size])
+		defer s.do(b[size:], action, getSlot)
+		action(s.slots[len(s.slots)-1][s.pos:], b[:size])
 		s.pos += size
 		return
 	}
 
-	copy(s.slots[len(s.slots)-1][s.pos:s.pos+len(b)], b)
+	action(s.slots[len(s.slots)-1][s.pos:s.pos+len(b)], b)
 	s.pos += len(b)
+}
+
+func (s *Storage) Flush() {
 }
