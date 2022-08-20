5b636cd72c6f0c7e4b669686e78b61f139680662
dkornet <kornet@waterfall.foundation>
2022-08-20 11:02:53 +0300

    fix: nil txList in demoteUnexecutables

--- core/tx_pool.go
+++ core/tx_pool.go
@@ -1658,7 +1658,7 @@ func (pool *TxPool) demoteUnexecutables() {
 
 		// If there's a gap in front, alert (should never happen) and postpone all transactions
 		if list.Len() > 0 && list.txs.Get(nonce) == nil {
-			if listPF.Len() > 0 && listPF.txs.Get(nonce) == nil {
+			if listPF != nil && listPF.Len() > 0 && listPF.txs.Get(nonce) == nil {
 				gapped := list.Cap(0)
 				for _, tx := range gapped {
 					hash := tx.Hash()
