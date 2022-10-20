851d4a2a8bffd5604d5525fe8c5a4d6832b08e5b
dkornet <kornet@waterfall.foundation>
2022-10-20 17:04:58 +0300

    fix: nonce calculation bug

--- core/tx_pool.go
+++ core/tx_pool.go
@@ -1467,10 +1467,15 @@ func (pool *TxPool) runReorg(done chan struct{}, reset *txpoolResetRequest, dirt
 	pool.truncateQueue()
 
 	// Update all accounts to the latest known pending nonce
+	for addr, list := range pool.processing {
+		highestPending := list.LastElement()
+		pool.pendingNonces.setIfGreater(addr, highestPending.Nonce()+1)
+	}
 	for addr, list := range pool.pending {
 		highestPending := list.LastElement()
-		pool.pendingNonces.set(addr, highestPending.Nonce()+1)
+		pool.pendingNonces.setIfGreater(addr, highestPending.Nonce()+1)
 	}
+
 	//dropBetweenReorgHistogram.Update(int64(pool.changesSinceReorg))
 	//pool.changesSinceReorg = 0 // Reset change counter
 	pool.mu.Unlock()
