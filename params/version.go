6018e30f6032459929b6938f19299edb69d59ee3
Vladimir Mezin <vm@waterfall.foundation>
2023-01-08 09:59:15 +0200

    build: v0.6.5

--- params/version.go
+++ params/version.go
@@ -27,7 +27,7 @@ const BuildId = "\u2692"
 const (
 	VersionMajor = 0          // Major version component of the current release
 	VersionMinor = 6          // Minor version component of the current release
-	VersionPatch = 2          // Patch version component of the current release
+	VersionPatch = 5          // Patch version component of the current release
 	VersionMeta  = "unstable" // Version metadata to append to the version string
 )
 
