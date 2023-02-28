92b8f7078e93b208a25821bc182d9fc044144746
Vladimir Mezin <vm@waterfall.foundation>
2023-02-28 18:08:02 +0200

    feat: validator sync structs

--- core/types/dag.go
+++ core/types/dag.go
@@ -1,7 +1,10 @@
 package types
 
 import (
+	"encoding/binary"
 	"encoding/json"
+	"fmt"
+	"math/big"
 
 	"gitlab.waterfall.network/waterfall/protocol/gwat/common"
 	"gitlab.waterfall.network/waterfall/protocol/gwat/common/hexutil"
@@ -56,10 +59,181 @@ func (ci *ConsensusInfo) UnmarshalJSON(input []byte) error {
 	return nil
 }
 
+// Checkpoint represents a coordinated checkpoint
+// of coorinator and gwat nodes
+type Checkpoint struct {
+	Epoch uint64
+	Root  common.Hash
+	Spine common.Hash
+}
+
+type checkpointMarshaling struct {
+	Epoch *hexutil.Uint64 `json:"epoch"`
+	Root  *common.Hash    `json:"root"`
+	Spine *common.Hash    `json:"spine"`
+}
+
+// Bytes gets the byte representation.
+func (cp *Checkpoint) Bytes() []byte {
+	cpLen := 8 + common.HashLength + common.HashLength
+	res := make([]byte, 0, cpLen)
+	epoch := make([]byte, 8)
+	binary.BigEndian.PutUint64(epoch, cp.Epoch)
+	res = append(res, epoch...)
+	res = append(res, cp.Root.Bytes()...)
+	res = append(res, cp.Spine.Bytes()...)
+	return res
+}
+
+func (cp *Checkpoint) SetBytes(data []byte) error {
+	cpLen := 8 + common.HashLength + common.HashLength
+	if len(data) != cpLen {
+		return fmt.Errorf("bad bitlen: got=%d req=%d", len(data), cpLen)
+	}
+	var start, end int
+	start = 0
+	end += 8
+	cp.Epoch = binary.BigEndian.Uint64(data[start:end])
+
+	start = end
+	end += common.HashLength
+	cp.Root = common.BytesToHash(data[start:end])
+
+	start = end
+	end += common.HashLength
+	cp.Spine = common.BytesToHash(data[start:end])
+
+	return nil
+}
+
+// BytesToCheckpoint create Checkpoint from bytes.
+func BytesToCheckpoint(b []byte) (*Checkpoint, error) {
+	var h Checkpoint
+	err := h.SetBytes(b)
+	if err != nil {
+		return nil, err
+	}
+	return &h, err
+}
+
+func (cp *Checkpoint) Copy() *Checkpoint {
+	cpy := &Checkpoint{
+		Epoch: cp.Epoch,
+		Spine: cp.Spine,
+	}
+	copy(cpy.Root[:], cp.Root[:])
+	copy(cpy.Spine[:], cp.Spine[:])
+	return cpy
+}
+
+func (cp *Checkpoint) MarshalJSON() ([]byte, error) {
+	out := checkpointMarshaling{
+		Epoch: (*hexutil.Uint64)(&cp.Epoch),
+		Root:  &cp.Root,
+		Spine: &cp.Spine,
+	}
+	return json.Marshal(out)
+}
+
+func (cp *Checkpoint) UnmarshalJSON(input []byte) error {
+	var dec checkpointMarshaling
+	if err := json.Unmarshal(input, &dec); err != nil {
+		return err
+	}
+	if dec.Epoch != nil {
+		cp.Epoch = uint64(*dec.Epoch)
+	}
+	if dec.Root != nil {
+		cp.Root = *dec.Root
+	}
+	if dec.Spine != nil {
+		cp.Spine = *dec.Spine
+	}
+	return nil
+}
+
+type ValidatorSyncOp uint64
+
+const (
+	Activation ValidatorSyncOp = iota
+	Exit
+	Withdrawal
+)
+
+// ValidatorSync represents a data to perform operation of validators synchronization
+// from coordinator
+type ValidatorSync struct {
+	OpType    ValidatorSyncOp
+	ProcEpoch uint64
+	Index     uint64
+	Creator   common.Address
+	Amount    *big.Int
+}
+
+type validatorSyncMarshaling struct {
+	OpType    *hexutil.Uint64 `json:"opType"`
+	ProcEpoch *hexutil.Uint64 `json:"procEpoch"`
+	Index     *hexutil.Uint64 `json:"index"`
+	Creator   *common.Address `json:"creator"`
+	Amount    *hexutil.Big    `json:"amount"`
+}
+
+func (vs *ValidatorSync) Copy() *ValidatorSync {
+	cpy := &ValidatorSync{
+		OpType:    vs.OpType,
+		ProcEpoch: vs.ProcEpoch,
+		Index:     vs.Index,
+	}
+	copy(cpy.Creator[:], vs.Creator[:])
+	if vs.Amount != nil {
+		cpy.Amount = new(big.Int).Set(vs.Amount)
+	}
+	return cpy
+}
+
+func (vs *ValidatorSync) MarshalJSON() ([]byte, error) {
+	out := validatorSyncMarshaling{
+		OpType:    (*hexutil.Uint64)(&vs.OpType),
+		ProcEpoch: (*hexutil.Uint64)(&vs.ProcEpoch),
+		Index:     (*hexutil.Uint64)(&vs.Index),
+		Creator:   &vs.Creator,
+		Amount:    nil,
+	}
+	if vs.Amount != nil {
+		out.Amount = (*hexutil.Big)(vs.Amount)
+	}
+	return json.Marshal(out)
+}
+
+func (vs *ValidatorSync) UnmarshalJSON(input []byte) error {
+	var dec validatorSyncMarshaling
+	if err := json.Unmarshal(input, &dec); err != nil {
+		return err
+	}
+	if dec.OpType != nil {
+		vs.OpType = ValidatorSyncOp(*dec.OpType)
+	}
+	if dec.ProcEpoch != nil {
+		vs.ProcEpoch = uint64(*dec.ProcEpoch)
+	}
+	if dec.Index != nil {
+		vs.Index = uint64(*dec.Index)
+	}
+	if dec.Creator != nil {
+		vs.Creator = *dec.Creator
+	}
+	if dec.Amount != nil {
+		vs.Amount = (*big.Int)(dec.Amount)
+	}
+	return nil
+}
+
 // FinalizationParams represents params of finalization request
 type FinalizationParams struct {
-	Spines    common.HashArray `json:"spines"`
-	BaseSpine *common.Hash     `json:"baseSpine"`
+	Spines        common.HashArray `json:"spines"`
+	BaseSpine     *common.Hash     `json:"baseSpine"`
+	Checkpoint    *Checkpoint      `json:"checkpoint"`
+	ValidatorSync *ValidatorSync   `json:"validatorSync"`
 }
 
 // Copy duplicates the current storage.
@@ -70,6 +244,12 @@ func (fp *FinalizationParams) Copy() *FinalizationParams {
 	if fp.BaseSpine != nil {
 		cpy.BaseSpine = &common.Hash{}
 		copy(cpy.BaseSpine[:], fp.BaseSpine[:])
+	}
+	if fp.Checkpoint != nil {
+		cpy.Checkpoint = fp.Checkpoint.Copy()
+	}
+	if fp.ValidatorSync != nil {
+		cpy.ValidatorSync = fp.ValidatorSync.Copy()
 	}
 	return cpy
 }
@@ -90,6 +270,12 @@ func (fp *FinalizationParams) UnmarshalJSON(input []byte) error {
 	}
 	if dec.BaseSpine != nil {
 		fp.BaseSpine = dec.BaseSpine
+	}
+	if dec.Checkpoint != nil {
+		fp.Checkpoint = dec.Checkpoint
+	}
+	if dec.ValidatorSync != nil {
+		fp.ValidatorSync = dec.ValidatorSync
 	}
 	return nil
 }
