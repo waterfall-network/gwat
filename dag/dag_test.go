064c5299c3d73010c07cd3ec53eea2b9291466c0
Rogovoi Dmitrii <rogovoi.dmitrii.a@gmail.com>
2023-04-06 22:04:42 +0300

    test: dag

--- dag/dag_test.go
+++ dag/dag_test.go
@@ -1,9 +1,69 @@
 package dag
 
 import (
+	"sync"
 	"testing"
+	"time"
+
+	"github.com/golang/mock/gomock"
+	"gitlab.waterfall.network/waterfall/protocol/gwat/common"
+	"gitlab.waterfall.network/waterfall/protocol/gwat/core/rawdb"
+	"gitlab.waterfall.network/waterfall/protocol/gwat/core/types"
+	"gitlab.waterfall.network/waterfall/protocol/gwat/dag/creator"
+	"gitlab.waterfall.network/waterfall/protocol/gwat/eth/downloader"
+	"gitlab.waterfall.network/waterfall/protocol/gwat/ethdb"
+	"gitlab.waterfall.network/waterfall/protocol/gwat/tests/testutils"
 )
 
 func TestGetOptimisticSpines(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	bc := NewMockblockChain(ctrl)
+	db := rawdb.NewMemoryDatabase()
+	prepareTestBlocks(t, db)
 
+	down := NewMockethDownloader(ctrl)
+	down.EXPECT().Synchronising().Return(false)
+
+	backend := NewMockBackend(ctrl)
+	backend.EXPECT().Downloader().Return(&downloader.Downloader{})
+
+	bc.EXPECT().Database().AnyTimes().Return(db)
+	bc.EXPECT().GetBlock(spineBlock.Hash()).Return(spineBlock)
+	bc.EXPECT().GetSlotInfo().Return(&types.SlotInfo{
+		GenesisTime:    uint64(time.Now().Unix() - 100),
+		SecondsPerSlot: 4,
+		SlotsPerEpoch:  32,
+	})
+
+	for _, testBlock := range testBlocks {
+		bc.EXPECT().GetBlock(testBlock.Hash()).Return(testBlock)
+	}
+
+	dag := Dag{eth: backend, bc: bc, downloader: down, mutex: new(sync.Mutex)}
+	result := dag.HandleGetOptimisticSpines(spineBlock.Hash())
+
+	expectedResult := types.OptimisticSpinesResult{
+		Data:  []common.HashArray{{block.Hash()}, {block3.Hash()}, {block6.Hash(), block5.Hash()}, {block7.Hash()}},
+		Error: nil,
+	}
+	testutils.AssertEqual(t, expectedResult, *result)
+
+	//	Check if downloader is synchronising
+	bc.EXPECT().GetBlock(spineBlock.Hash()).Return(spineBlock)
+	down.EXPECT().Synchronising().Return(true)
+	result = dag.HandleGetOptimisticSpines(spineBlock.Hash())
+	expectedErr := creator.ErrSynchronization.Error()
+	expectedResult = types.OptimisticSpinesResult{
+		Error: &expectedErr,
+	}
+	testutils.AssertEqual(t, expectedResult, *result)
+}
+
+func prepareTestBlocks(t *testing.T, db ethdb.Database) {
+	blocksBySlots, err := testBlocks.GroupBySlot()
+	testutils.AssertNoError(t, err)
+
+	for slot, blocks := range blocksBySlots {
+		rawdb.WriteSlotBlocksHashes(db, slot, *blocks.GetHashes())
+	}
 }
---
+++ dag/test_models.go
@@ -0,0 +1,94 @@
+package dag
+
+import (
+	"gitlab.waterfall.network/waterfall/protocol/gwat/common"
+	"gitlab.waterfall.network/waterfall/protocol/gwat/core/types"
+	"gitlab.waterfall.network/waterfall/protocol/gwat/tests/testutils"
+)
+
+var (
+	spineBlock = types.NewBlock(&types.Header{
+		Slot:   11,
+		Height: 11,
+		TxHash: common.BytesToHash(testutils.RandomData(32)),
+		Root:   common.BytesToHash(testutils.RandomData(32)),
+	}, nil, nil, nil)
+
+	block = types.NewBlock(&types.Header{
+		Slot:   12,
+		Height: 13,
+		TxHash: common.BytesToHash(testutils.RandomData(32)),
+		Root:   common.BytesToHash(testutils.RandomData(32)),
+	}, nil, nil, nil)
+
+	block2 = types.NewBlock(&types.Header{
+		Slot:   12,
+		Height: 12,
+		TxHash: common.BytesToHash(testutils.RandomData(32)),
+		Root:   common.BytesToHash(testutils.RandomData(32)),
+	}, nil, nil, nil)
+
+	block3 = types.NewBlock(&types.Header{
+		Slot:   15,
+		Height: 14,
+		ParentHashes: common.HashArray{
+			common.BytesToHash(testutils.RandomData(32)),
+			common.BytesToHash(testutils.RandomData(32)),
+			common.BytesToHash(testutils.RandomData(32)),
+			common.BytesToHash(testutils.RandomData(32)),
+		},
+		TxHash: common.BytesToHash(testutils.RandomData(32)),
+		Root:   common.BytesToHash(testutils.RandomData(32)),
+	}, nil, nil, nil)
+
+	block4 = types.NewBlock(&types.Header{
+		Slot:   15,
+		Height: 14,
+		ParentHashes: common.HashArray{
+			common.BytesToHash(testutils.RandomData(32)),
+			common.BytesToHash(testutils.RandomData(32)),
+		},
+		TxHash: common.BytesToHash(testutils.RandomData(32)),
+		Root:   common.BytesToHash(testutils.RandomData(32)),
+	}, nil, nil, nil)
+
+	block5 = types.NewBlock(&types.Header{
+		Slot:   16,
+		Height: 10,
+		ParentHashes: common.HashArray{
+			common.Hash{0x01},
+			common.Hash{0x02},
+			common.Hash{0x03},
+			common.Hash{0x04},
+			common.Hash{0x05},
+		},
+		TxHash: common.Hash{0x025, 255, 255},
+		Root:   common.Hash{0x025, 255, 255},
+	}, nil, nil, nil)
+
+	block6 = types.NewBlock(&types.Header{
+		Slot:   16,
+		Height: 10,
+		ParentHashes: common.HashArray{
+			common.Hash{0x07},
+			common.Hash{0x08},
+			common.Hash{0x09},
+			common.Hash{0x10},
+			common.Hash{0x11},
+		},
+		TxHash: common.Hash{0x01},
+		Root:   common.Hash{0x01},
+	}, nil, nil, nil)
+
+	block7 = types.NewBlock(&types.Header{
+		Slot:   22,
+		Height: 35,
+		ParentHashes: common.HashArray{
+			common.Hash{0x01},
+		},
+		TxHash: common.Hash{0x025, 255, 255},
+		Root:   common.Hash{0x025, 255, 255},
+	}, nil, nil, nil)
+
+	testBlocks = types.Blocks{block, block2, block3, block4, block5, block6, block7}
+)
