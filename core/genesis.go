0ef1bf23f24adad4abb4cdcff19b9a18355379bb
Dmitriy Rogovoi <rogovoi@waterfall.foundation>
2023-08-28 14:00:10 +0300

    rename blockDag field.

--- core/genesis.go
+++ core/genesis.go
@@ -392,12 +392,12 @@ func (g *Genesis) Commit(db ethdb.Database) (*types.Block, error) {
 	rawdb.AddSlotBlockHash(db, block.Slot(), block.Hash())
 	//set genesis blockDag
 	genesisDag := &types.BlockDAG{
-		Hash:           block.Hash(),
-		Height:         0,
-		Slot:           0,
-		CpHash:         block.Hash(),
-		CpHeight:       0,
-		DagChainHashes: common.HashArray{},
+		Hash:                   block.Hash(),
+		Height:                 0,
+		Slot:                   0,
+		CpHash:                 block.Hash(),
+		CpHeight:               0,
+		OrderedAncestorsHashes: common.HashArray{},
 	}
 	rawdb.WriteBlockDag(db, genesisDag)
 	rawdb.WriteTipsHashes(db, common.HashArray{genesisDag.Hash})
