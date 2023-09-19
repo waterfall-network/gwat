bba33eb52b0b85958e53c94efdda029a61681993
YuriTkachyshyn <yuri.tkachyshyn@waterfall.foundation>
2023-09-19 17:53:02 +0300

    refactor

--- dag/creator/utils.go
+++ dag/creator/utils.go
@@ -3,10 +3,7 @@ package creator
 import (
 	"errors"
 	"fmt"
-	"io"
 	"io/ioutil"
-	"os"
-	"runtime"
 	"strconv"
 	"strings"
 
@@ -42,29 +39,9 @@ func MakeAddress(ks *keystore.KeyStore, account string) (accounts.Account, error
 	return accs[index], nil
 }
 
-// Fatalf formats a message to standard error and exits the program.
-// The message is also printed to standard output if standard error
-// is redirected to a different file.
-func Fatalf(format string, args ...interface{}) {
-	w := io.MultiWriter(os.Stdout, os.Stderr)
-	if runtime.GOOS == "windows" {
-		// The SameFile check below doesn't work on Windows.
-		// stdout is unlikely to get redirected though, so just print there.
-		w = os.Stdout
-	} else {
-		outf, _ := os.Stdout.Stat()
-		errf, _ := os.Stderr.Stat()
-		if outf != nil && errf != nil && os.SameFile(outf, errf) {
-			w = os.Stderr
-		}
-	}
-	fmt.Fprintf(w, "Fatal: "+format+"\n", args...)
-	os.Exit(1)
-}
-
 // GetPassPhraseWithList retrieves the password associated with an account, either fetched
 // from a list of preloaded passphrases, or requested interactively from the user.
-func GetPassPhraseWithList(text string, confirmation bool, index int, passwords []string) string {
+func getPassPhraseWithList(confirmation bool, index int, passwords []string) string {
 	// If a list of passwords was supplied, retrieve from them
 	if len(passwords) > 0 {
 		if index < len(passwords) {
@@ -73,28 +50,25 @@ func GetPassPhraseWithList(text string, confirmation bool, index int, passwords 
 		return passwords[len(passwords)-1]
 	}
 	// Otherwise prompt the user for the password
-	password := GetPassPhrase(text, confirmation)
+	password := getPassPhrase(confirmation)
 	return password
 }
 
 // GetPassPhrase displays the given text(prompt) to the user and requests some textual
 // data to be entered, but one which must not be echoed out into the terminal.
 // The method returns the input provided by the user.
-func GetPassPhrase(text string, confirmation bool) string {
-	if text != "" {
-		fmt.Println(text)
-	}
+func getPassPhrase(confirmation bool) string {
 	password, err := prompt.Stdin.PromptPassword("Password: ")
 	if err != nil {
-		Fatalf("Failed to read password: %v", err)
+		log.Error("Failed to read password:", "err", err)
 	}
 	if confirmation {
 		confirm, err := prompt.Stdin.PromptPassword("Repeat password: ")
 		if err != nil {
-			Fatalf("Failed to read password confirmation: %v", err)
+			log.Error("Failed to read password confirmation:", "err", err)
 		}
 		if password != confirm {
-			Fatalf("Passwords do not match")
+			log.Error("Passwords do not match")
 		}
 	}
 	return password
@@ -111,68 +85,33 @@ func findAccountPosition(accounts []accounts.Account, targetAddress string) int 
 }
 
 // tries unlocking the specified account a few times.
-func unlockAccount(ks *keystore.KeyStore, address string, pos int, passwords []string) (accounts.Account, string) {
+func unlockAccount(ks *keystore.KeyStore, address string, pos int, passwords []string) error {
 	account, err := MakeAddress(ks, address)
 	if err != nil {
-		Fatalf("Could not list accounts: %v", err)
+		log.Error("Could not list accounts", "error", err)
+		return err
 	}
+
 	for trials := 0; trials < 3; trials++ {
-		prompt := fmt.Sprintf("Unlocking account %s | Attempt %d/%d", address, trials+1, 3)
-
-		password := GetPassPhraseWithList(prompt, false, pos, passwords)
-		prompt1 := fmt.Sprintf("Unlocking account %s | Attempt %d/%d, | Password %s", address, trials+1, 3, passwords)
-		log.Info(prompt1)
+		password := getPassPhraseWithList(false, pos, passwords)
 		err = ks.Unlock(account, password)
-		if err == nil {
-			log.Info("Unlocked account", "address", account.Address.Hex())
-			return account, password
-		}
-		if err, ok := err.(*keystore.AmbiguousAddrError); ok {
-			log.Info("Unlocked account", "address", account.Address.Hex())
-			return ambiguousAddrRecovery(ks, err, password), password
-		}
-		if err != keystore.ErrDecrypt {
-			// No need to prompt again if the error is not decryption-related.
-			break
+		if err != nil {
+			log.Warn("Failed to unlock account", "account", address, "error", err)
+			return err
 		}
 	}
-	// All trials expended to unlock account, bail out
-	Fatalf("Failed to unlock account %s (%v)", address, err)
 
-	return accounts.Account{}, ""
-}
+	log.Info("Unlocked account", "address", account.Address.Hex())
 
-func ambiguousAddrRecovery(ks *keystore.KeyStore, err *keystore.AmbiguousAddrError, auth string) accounts.Account {
-	fmt.Printf("Multiple key files exist for address %x:\n", err.Addr)
-	for _, a := range err.Matches {
-		fmt.Println("  ", a.URL)
-	}
-	fmt.Println("Testing your password against all of them...")
-	var match *accounts.Account
-	for _, a := range err.Matches {
-		if err := ks.Unlock(a, auth); err == nil {
-			match = &a
-			break
-		}
-	}
-	if match == nil {
-		Fatalf("None of the listed files could be unlocked.")
-	}
-	fmt.Printf("Your password unlocked %s\n", match.URL)
-	fmt.Println("In order to avoid this warning, you need to remove the following duplicate key files:")
-	for _, a := range err.Matches {
-		if a != *match {
-			fmt.Println("  ", a.URL)
-		}
-	}
-	return *match
+	return nil
 }
 
 // MakePasswordList reads password lines from the file specified by the global --password flag.
 func makePasswordList(path string) []string {
 	text, err := ioutil.ReadFile(path)
 	if err != nil {
-		Fatalf("Failed to read password file: %v", err)
+		log.Error("Failed to read password file", "error", err)
+		return nil
 	}
 	lines := strings.Split(string(text), "\n")
 	// Sanitise DOS line endings.
